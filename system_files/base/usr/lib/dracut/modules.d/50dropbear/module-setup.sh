a59510251bdbb3b4da86865f3e42d639a9dc1c28
bdfd9 <dpaprocki+github@proton.me>
2026-08-23 00:20:36 +0200

    .

--- system_files/base/usr/lib/dracut/modules.d/50dropbear/module-setup.sh
+++ system_files/base/usr/lib/dracut/modules.d/50dropbear/module-setup.sh
@@ -17,9 +17,13 @@ install() {
     inst_simple "$moddir/dropbear-ssh.service" "$systemdsystemunitdir/dropbear-ssh.service"
     inst_simple "$moddir/dropbear-ssh" "/usr/sbin/dropbear-ssh"
 
+
+    systemctl -q --root "$initdir" add-requires "prepare-dropbear-keys.service" "initrd.target" || exit 1
+    systemctl -q --root "$initdir" add-requires "dropbear-ssh.service" "initrd.target" || exit 1
+
     systemctl -q --root "$initdir" add-wants "prepare-dropbear-keys.service" "initrd.target" || exit 1
     systemctl -q --root "$initdir" add-wants "dropbear-ssh.service" "prepare-dropbear-keys.service" || exit 1
 
-    systemctl -q --root "$initdir" enable prepare-dropbear-keys.service
-    systemctl -q --root "$initdir" enable dropbear-ssh.service
+    # systemctl -q --root "$initdir" enable prepare-dropbear-keys.service
+    # systemctl -q --root "$initdir" enable dropbear-ssh.service
 }
