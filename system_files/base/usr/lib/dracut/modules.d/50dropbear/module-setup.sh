133775f2a0aea46e1fbb8eb3d85cc2397dbf7496
bdfd9 <dpaprocki+github@proton.me>
2026-08-23 00:28:18 +0200

    .

--- system_files/base/usr/lib/dracut/modules.d/50dropbear/module-setup.sh
+++ system_files/base/usr/lib/dracut/modules.d/50dropbear/module-setup.sh
@@ -11,18 +11,18 @@ depends() {
 install() {
     inst dropbear
 
-    inst_simple "$moddir/prepare-dropbear-keys.service" "$systemdsystemunitdir/prepare-dropbear-keys.service"
-    inst_simple "$moddir/prepare-dropbear-keys" "/usr/sbin/prepare-dropbear-keys"
+    inst_simple "$moddir/prepare-dropbear-keys.service" "$systemdsystemunitdir/dropbear-prepare-keys.service"
+    inst_simple "$moddir/prepare-dropbear-keys" "/usr/sbin/dropbear-prepare-keys"
 
     inst_simple "$moddir/dropbear-ssh.service" "$systemdsystemunitdir/dropbear-ssh.service"
     inst_simple "$moddir/dropbear-ssh" "/usr/sbin/dropbear-ssh"
 
 
-    systemctl -q --root "$initdir" add-requires "prepare-dropbear-keys.service" "initrd.target" || exit 1
+    systemctl -q --root "$initdir" add-requires "dropbear-prepare-keys.service" "initrd.target" || exit 1
     systemctl -q --root "$initdir" add-requires "dropbear-ssh.service" "initrd.target" || exit 1
 
-    systemctl -q --root "$initdir" add-wants "prepare-dropbear-keys.service" "initrd.target" || exit 1
-    systemctl -q --root "$initdir" add-wants "dropbear-ssh.service" "prepare-dropbear-keys.service" || exit 1
+    systemctl -q --root "$initdir" add-wants "dropbear-prepare-keys.service" "initrd.target" || exit 1
+    systemctl -q --root "$initdir" add-wants "dropbear-ssh.service" "dropbear-prepare-keys.service" || exit 1
 
     # systemctl -q --root "$initdir" enable prepare-dropbear-keys.service
     # systemctl -q --root "$initdir" enable dropbear-ssh.service
