6af62dbc1ebd555a0a4f65069012e8ccb6542efe
bdfd9 <dpaprocki+github@proton.me>
2026-08-23 02:14:02 +0200

    .

--- system_files/base/usr/lib/dracut/modules.d/50dropbear/module-setup.sh
+++ system_files/base/usr/lib/dracut/modules.d/50dropbear/module-setup.sh
@@ -10,23 +10,24 @@ depends() {
 
 install() {
     inst dropbear
+    inst dropbearkey
 
     inst_simple "$moddir/dropbear-prepare-keys.service" "$systemdsystemunitdir/dropbear-prepare-keys.service"
-    inst_simple "$moddir/dropbear-prepare-keys" "/usr/sbin/dropbear-prepare-keys"
+    inst_simple "$moddir/dropbear-prepare-keys" "/usr/bin/dropbear-prepare-keys"
 
     inst_simple "$moddir/dropbear-ssh.service" "$systemdsystemunitdir/dropbear-ssh.service"
-    inst_simple "$moddir/dropbear-ssh" "/usr/sbin/dropbear-ssh"
+    inst_simple "$moddir/dropbear-ssh" "/usr/bin/dropbear-ssh"
 
 
     # systemctl -q --root "$initdir" add-requires "dropbear-prepare-keys.service" "initrd.target" || exit 1
     # systemctl -q --root "$initdir" add-requires "dropbear-ssh.service" "initrd.target" || exit 1
 
-    systemctl -q --root "$initdir" add-requires "initrd.target" "dropbear-prepare-keys.service" || exit 1
-    systemctl -q --root "$initdir" add-requires "initrd.target" "dropbear-ssh.service" || exit 1
+    # systemctl -q --root "$initdir" add-requires "initrd.target" "dropbear-prepare-keys.service" || exit 1
+    # systemctl -q --root "$initdir" add-requires "initrd.target" "dropbear-ssh.service" || exit 1
 
-    systemctl -q --root "$initdir" add-wants "dropbear-prepare-keys.service" "initrd.target" || exit 1
-    systemctl -q --root "$initdir" add-wants "dropbear-ssh.service" "dropbear-prepare-keys.service" || exit 1
+    # systemctl -q --root "$initdir" add-wants "dropbear-prepare-keys.service" "initrd.target" || exit 1
+    # systemctl -q --root "$initdir" add-wants "dropbear-ssh.service" "dropbear-prepare-keys.service" || exit 1
 
-    # systemctl -q --root "$initdir" enable prepare-dropbear-keys.service
-    # systemctl -q --root "$initdir" enable dropbear-ssh.service
+    systemctl -q --root "$initdir" enable dropbear-prepare-keys.service
+    systemctl -q --root "$initdir" enable dropbear-ssh.service
 }
